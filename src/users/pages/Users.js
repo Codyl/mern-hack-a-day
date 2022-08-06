62a1f3b664c3b219215ca5efc61dc8de1351565c
Cody Lillywhite <codylillyw@gmail.com>
2022-08-05 23:22:49 -0600

    Modify users to get from mongodb

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,15 +1,44 @@
-import React from "react";
-import UsersList from "../components/UsersList";
+import React, { useEffect, useState } from 'react';
+import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElmements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElmements/LoadingSpinner';
 
 export const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "ben shallus",
-      image:
-        "https://image.shutterstock.com/image-photo/closeup-portrait-smiling-half-naked-600w-1356965294.jpg",
-      places: 3,
-    },
-  ];
-  return <UsersList items={USERS} />;
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      let responseData;
+      try {
+        const response = await fetch('localhost:5000/api/users');
+        responseData = response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+
+      setLoadedUsers(responseData.users);
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+  const errorHandler = () => {
+    setError(null);
+  };
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
 };
